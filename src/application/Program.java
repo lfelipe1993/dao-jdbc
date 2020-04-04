73664ee1a949ec2b431c346d4591e3c71e3f5792
Luiz Felipe Mecina Greijo <lfelipe1993@live.com>
2020-04-04 14:08:55 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,7 +28,6 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		
 		System.out.println("\n=== TEST 3: seller findAll ===");
 
 		list = sellerDao.findAll();
@@ -43,6 +42,17 @@ public class Program {
 		sellerDao.insert(newSeller);
 		
 		System.out.println("Inserted! New id= " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		
+		seller = sellerDao.findById(1);
+		
+		seller.setName("Martha Waine");
+
+		sellerDao.update(seller);
+		
+		System.out.println("Update Completed!");
+		
 
 	}
 
