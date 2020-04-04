7f3025dc05fd204997376ae1eec8727139bd4f91
Luiz Felipe Mecina Greijo <lfelipe1993@live.com>
2020-04-04 12:33:22 -0300

    Seller entity class

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -6,7 +6,7 @@ public class Department implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private Integer Id;
+	private Integer id;
 	private String name;
 	
 	public Department(){
@@ -14,16 +14,16 @@ public class Department implements Serializable {
 	}
 
 	public Department(Integer id, String name) {
-		Id = id;
+		this.id = id;
 		this.name = name;
 	}
 
 	public Integer getId() {
-		return Id;
+		return id;
 	}
 
 	public void setId(Integer id) {
-		Id = id;
+		this.id = id;
 	}
 
 	public String getName() {
@@ -38,7 +38,7 @@ public class Department implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -51,17 +51,17 @@ public class Department implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		if (Id == null) {
-			if (other.Id != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!Id.equals(other.Id))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Department [Id=" + Id + ", name=" + name + "]";
+		return "Department [Id=" + id + ", name=" + name + "]";
 	}
 
 }
